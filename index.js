adda797edf56c86311a7c5d205e1e9784bbd8681
Dark-Knigth-maker <78041177+Dark-Knigth-maker@users.noreply.github.com>
2021-07-02 12:33:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const morgan = require('morgan');
+const flash = require('connect-flash');
+const MySqlStore = require('express-mysql-session');
+const passport = require('passport');
+
+const { database } = require('./keys');
+
+// Initiliazations
+const app = express();
+require('./lib/passport');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layauts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engiene', '.hbs');
+
+// Middlewares
+app.use(session({
+    secret: 'mysecretapp',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySqlStore(database)
+}));
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Global Variables
+app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
+    app.locals.message = req.flash('message');
+    app.locals.user = req.user;
+    next();
+});
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
+app.use('/links', require('./routes/links'));
+
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Server is listenning
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    
+    database: {
+        host: 'bkvlc4eyw9nyhjehfm9s-mysql.services.clever-cloud.com',
+        user: 'uufbdm8rcjkhew1r',
+        password: 'VTpUxfNFJeGbkjAbgoxD',
+        database: 'bkvlc4eyw9nyhjehfm9s'
+    }
+};
